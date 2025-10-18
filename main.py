c658bae191b184ceef95960b22a78c2e755faf05
leotso38 <leotso@outlook.com>
2025-10-18 08:37:26 -0400

    fix all comments main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+# Author: Leo Tso
+# Date: 2025-10-18
+# Class: IS601
+# File: main.py
+# Notes: Entry point for launching the interactive Calculator REPL.
+
 from app.calculator_repl import calculator_repl
 
 if __name__ == "__main__":
-    from app.calculator_repl import calculator_repl
     calculator_repl()
